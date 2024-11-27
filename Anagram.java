6f1244266bd1fd5f222c0ec95fa28a76035a890c
benjaminbis <benjaminbismuth2@gmail.com>
2024-11-27 22:50:41 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,49 +1,57 @@
-/** Functions for checking if a given string is an anagram. */
 public class Anagram {
-	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+    public static void main(String[] args) {
+        System.out.println(isAnagram("silent", "listen"));
+        System.out.println(isAnagram("William Shakespeare", "I am a weakish speller"));
+        System.out.println(isAnagram("Madam Curie", "Radium came"));
+        System.out.println(preProcess("What? No way!!!"));
+        System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+        String str = "1234567";
+        boolean pass = true;
+        for (int i = 0; i < 10; i++) {
+            String randomAnagram = randomAnagram(str);
+            pass = pass && isAnagram(str, randomAnagram);
+            if (!pass) break;
+        }
+        System.out.println(pass ? "test passed" : "test failed");
+    }
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+    public static boolean isAnagram(String str1, String str2) {
+        str1 = preProcess(str1);
+        str2 = preProcess(str2);
+        if (str1.length() != str2.length()) return false;
+        for (int i = 0; i < str1.length(); i++) {
+            if (countOccurrences(str1, str1.charAt(i)) != countOccurrences(str2, str1.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	// Returns true if the two given strings are anagrams, false otherwise.
-	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
-	} 
-	   
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
-	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
-	}
+    public static String preProcess(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c)) result += Character.toLowerCase(c);
+        }
+        return result;
+    }
+
+    public static String randomAnagram(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            int randomIndex = (int) (Math.random() * chars.length);
+            char temp = chars[i];
+            chars[i] = chars[randomIndex];
+            chars[randomIndex] = temp;
+        }
+        return new String(chars);
+    }
+
+    public static int countOccurrences(String str, char c) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) count++;
+        }
+        return count;
+    }
 }

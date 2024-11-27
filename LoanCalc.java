c7f04437e63692ac9de3de5ce2f8369eb4482631
benjaminbis <benjaminbismuth2@gmail.com>
2024-11-27 22:55:26 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -20,32 +20,35 @@ public class LoanCalc {
     }
 
     private static double endBalance(double loan, double rate, int n, double payment) {
+        double balance = loan;
         for (int i = 0; i < n; i++) {
-            loan += loan * (rate / 100);
-            loan -= payment;
+            balance += balance * (rate / 100);
+            balance -= payment;
         }
-        return loan;
+        return balance;
     }
 
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
         double payment = 0;
         iterationCounter = 0;
-
         while (endBalance(loan, rate, n, payment) > 0) {
             payment += epsilon;
             iterationCounter++;
         }
-
         return payment;
     }
 
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
-        double low = 0, high = loan * (1 + rate / 100), payment = 0;
+        double low = 0;
+        double high = loan * (1 + rate / 100);
+        double payment = 0;
         iterationCounter = 0;
 
         while (high - low > epsilon) {
             payment = (low + high) / 2;
-            if (endBalance(loan, rate, n, payment) > 0) {
+            double balance = endBalance(loan, rate, n, payment);
+
+            if (balance > 0) {
                 low = payment;
             } else {
                 high = payment;

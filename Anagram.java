db4e2917adc5f2ded66862781aa06fe5554748c0
benjaminbis <benjaminbismuth2@gmail.com>
2024-11-27 23:04:51 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,6 +1,9 @@
 public class Anagram {
     public static void main(String[] args) {
         System.out.println(isAnagram("silent", "listen"));
+        System.out.println(isAnagram("William Shakespeare", "I am a weakish speller"));
+        System.out.println(isAnagram("Madam Curie", "Radium came"));
+        System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort"));
         System.out.println(preProcess("What? No way!!!"));
         System.out.println(randomAnagram("silent"));
     }
@@ -9,11 +12,17 @@ public class Anagram {
         str1 = preProcess(str1);
         str2 = preProcess(str2);
         if (str1.length() != str2.length()) return false;
+
+        int[] charCounts = new int[26];
         for (int i = 0; i < str1.length(); i++) {
-            if (countOccurrences(str1, str1.charAt(i)) != countOccurrences(str2, str1.charAt(i))) {
-                return false;
-            }
+            charCounts[str1.charAt(i) - 'a']++;
+            charCounts[str2.charAt(i) - 'a']--;
         }
+
+        for (int count : charCounts) {
+            if (count != 0) return false;
+        }
+
         return true;
     }
 
@@ -21,11 +30,7 @@ public class Anagram {
         String result = "";
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (Character.isLetter(c)) {
-                result += Character.toLowerCase(c);
-            } else if (c == ' ') {
-                result += c;
-            }
+            if (Character.isLetter(c)) result += Character.toLowerCase(c);
         }
         return result;
     }
@@ -39,13 +44,5 @@ public class Anagram {
             chars[randomIndex] = temp;
         }
         return new String(chars);
-    }
-
-    public static int countOccurrences(String str, char c) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == c) count++;
-        }
-        return count;
     }
 }

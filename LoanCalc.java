cdb1a132d7790b46c2b309a30fbdaa8611c6981e
benjaminbis <benjaminbismuth2@gmail.com>
2024-11-28 19:34:17 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,66 +1,66 @@
 public class LoanCalc {
 
-    static double epsilon = 0.001;  
-    static int iterationCounter;   
+	static double epsilon = 0.001;  
+	static int iterationCounter;    
 
-    public static void main(String[] args) {
-        double loan = Double.parseDouble(args[0]);
-        double rate = Double.parseDouble(args[1]);
-        int n = Integer.parseInt(args[2]);
+	public static void main(String[] args) {		
+		double loan = Double.parseDouble(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
 
-        System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
+		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
-        System.out.print("\nPeriodical payment, using brute force: ");
-        System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
-        System.out.println("number of iterations: " + iterationCounter);
+		System.out.print("\nPeriodical payment, using brute force: ");
+		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
+		System.out.println("number of iterations: " + iterationCounter);
 
-        System.out.print("\nPeriodical payment, using bi-section search: ");
-        System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
-        System.out.println("number of iterations: " + iterationCounter);
-    }
+		System.out.print("\nPeriodical payment, using bi-section search: ");
+		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
+		System.out.println("number of iterations: " + iterationCounter);
+	}
 
-    private static double endBalance(double loan, double rate, int n, double payment) {
-        double balance = loan;
-        double monthlyRate = rate / 100 / 12;
-        for (int i = 0; i < n; i++) {
-            balance += balance * monthlyRate;
-            balance -= payment;
-        }
-        return balance;
-    }
+	private static double endBalance(double loan, double rate, int n, double payment) {
+		double balance = loan;
+		rate /= 100;
+		for (int i = 0; i < n; i++) {
+			balance = (balance - payment) * (1 + rate);
+		}
+		return balance;
+	}
 
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-        double payment = loan / n;
-        iterationCounter = 0;
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+		iterationCounter = 0;
+		double payment = loan / n;
+		double increment = epsilon;
 
-        while (endBalance(loan, rate, n, payment) > 0) {
-            payment += epsilon;
-            iterationCounter++;
-            if (iterationCounter > 1e7) break;
-        }
+		while (true) {
+			double balance = endBalance(loan, rate, n, payment);
 
-        return payment;
-    }
+			if (balance <= 0) break;
+			payment += increment;
+			iterationCounter++;
+		}
 
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
-        double low = 0;
-        double high = loan;
-        double monthlyRate = rate / 100 / 12;
-        double payment = 0;
-        iterationCounter = 0;
+		return payment;
+	}
 
-        while (high - low > epsilon) {
-            payment = (low + high) / 2;
-            double balance = endBalance(loan, rate, n, payment);
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		iterationCounter = 0;
+		double lo = loan / n;
+		double hi = loan;
+		double mid;
 
-            if (balance > 0) {
-                low = payment;
-            } else {
-                high = payment;
-            }
-            iterationCounter++;
-        }
+		while (hi - lo > epsilon) {
+			mid = (lo + hi) / 2;
+			double balance = endBalance(loan, rate, n, mid);
 
-        return (low + high) / 2;
-    }
+			if (balance > epsilon) lo = mid;
+			else if (balance < -epsilon) hi = mid;
+			else return mid;
+
+			iterationCounter++;
+		}
+
+		return (lo + hi) / 2;
+	}
 }
